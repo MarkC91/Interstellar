29fe4f72ee0e88b27d9cbd68e5db7aba3ddb5d39
MarkC91 <markcyr9@hotmail.com>
2025-02-28 11:49:04 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
-const config = {
-  challenge: false, // Set to true if you want to enable password protection.
-  users: {
+const config=true pnpm start
+  challenge: true, // Set to true if you want to enable password protection.
+  users: MarkC91
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
-  },
-};
-
+    interstellar: "5364",
 export default config;
+git clone https://github.com/UseInterstellar/Interstellar
+cd Interstellar
+pnpm i
+pnpm start
+cd Interstellar
+git pull --force --allow-unrelated-histories # This may overwrite your local changes
